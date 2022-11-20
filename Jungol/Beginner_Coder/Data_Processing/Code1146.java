d300ff6aa4561d685e7d32e2a81a3da085d6e887
math-shpark <psuhoo122065@gmail.com>
2022-11-20 20:59:46 +0900

    [APS][Jungol] Solution of Code 1146

---
+++ Jungol/Beginner_Coder/Data_Processing/Code1146.java
@@ -0,0 +1,54 @@
+package APS.Jungol.Beginner_Coder.Data_Processing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * Code1146
+ */
+public class Code1146 {
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int length = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[length];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < length - 1; i++) {
+            int min = arr[i];
+            int minIdx = i;
+
+            for (int j = i + 1; j < length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    minIdx = j;
+                }
+            }
+
+            if (minIdx != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIdx];
+                arr[minIdx] = temp;
+            }
+
+            for (int j = 0; j < length; j++) {
+                System.out.print(arr[j] + " ");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
